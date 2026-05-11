826a556818755531ee63424ca73b115b3b4436d2
skittery268 <sabadzidzikashvili6@gmail.com>
2026-05-11 17:38:24 +0400

    Fix problem

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -17,7 +17,7 @@ const sendToken = (res, user) => {
         maxAge: process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: false,
-        sameSite: "Lax"
+        sameSite: "none"
     })
 
     user.password = undefined;
